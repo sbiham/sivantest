ee3b4968e9dc3db3159588cd35d3f963861cee1c
sbiham <34643811+sbiham@users.noreply.github.com>
2026-02-17 20:11:42 +0200

    Update tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from "tailwindcss";
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#000000',
+      }
+    },
+  },
+  plugins: [],
+} satisfies Config;
